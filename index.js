aaf8a9cae431b890a071e96c6493402f2dc7f4c3
dbf222 <70982231+dbf222@users.noreply.github.com>
2021-08-06 16:01:15 -0400

    Update index.js
    Code Along from Class weds 4th

--- index.js
+++ index.js
@@ -1,135 +1,117 @@
-  // // Convert a var declaration to const or let where appropriate.
-  // 2.  Convert a promise-based function (a function call with .then) to instead use async/await.
-  // 3.  Convert a function declaration into an arrow function.
-  // 4.  Convert a string concatenation to instead use template literals and string interpolation.
-  // 5.  Convert some object-related code to use ES6 destructuring
-  // //
 
-
-
-// capture references to important DOM elements
 const weatherContainer = document.getElementById('weather');
 const formEl = document.querySelector('form');
 const inputEl = document.querySelector('input');
 
 
-formEl.onsubmit = function(e) => {
-  // prevent the page from refreshing
+formEl.onsubmit = async e => {
   e.preventDefault();
-
-  // capture user's input from form field
-  const userInput = inputEl.value.trim()
-  // abort API call if user entered no value
+  var userInput = inputEl.value.trim()
   if (!userInput) return
-  // call the API and then update the page
-  getWeather(userInput)
-    .then(displayWeatherInfo)
-    .catch(displayLocNotFound)
+  try {
+  const weatherInfo = await getWeather(userInput)
+  displayWeatherInfo(weatherInfo)
+  } catch (err) {
+    displayLocNotFound()
+  }
 
-  // reset form field to a blank state
+
   inputEl.value = ""
 }
 
-// calls the OpenWeather API and returns an object of weather info
-async function getWeather(query) {
-  // default search to USA
+function getWeather(query) {
   if (!query.includes(",")) query += ',us'
-  // return the fetch call which returns a promise
-  // allows us to call .then on this function
-  var res = await
-   fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    query +
-    '&units=imperial&appid=6efff70fe1477748e31c17d1c504635f'
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&appid=6efff70fe1477748e31c17d1c504635f`
   )
     .then(function(res) {
       return res.json()
     })
     .then(function(data) {
-      // location not found, throw error/reject promise
+      const {
+        cod,
+        main: {
+          temp,
+          feels_like
+        },
+        weather: [
+          {
+            icon,
+            description
+          }
+        ]
+      } = data
+
       if (data.cod === "404") throw new Error('location not found')
-      // create weather icon URL
-      const iconUrl = 'https://openweathermap.org/img/wn/' +
+      var iconUrl = 'https://openweathermap.org/img/wn/' +
         data.weather[0].icon +
         '@2x.png'
-      const description = data.weather[0].description
-      const actualTemp = data.main.temp
-      const feelsLikeTemp = data.main.feels_like
-      const place = data.name + ", " + data.sys.country
-      // create JS date object from Unix timestamp
-      const updatedAt = new Date(data.dt * 1000)
-
-
-      // this object is used by displayWeatherInfo to update the HTML
+      var actualTemp = temp
+      var feelsLikeTemp = feels_like
+      var place = data.name + ", " + data.sys.country
+      var updatedAt = new Date(data.dt * 1000)
       return {
         coords: data.coord.lat + ',' + data.coord.lon,
-        description,
-     iconUrl,
-         actualTemp,
-      feelsLikeTemp,
-      place,
-       updatedAt
+        description: description,
+        iconUrl: iconUrl,
+        actualTemp: actualTemp,
+        feelsLikeTemp: feelsLikeTemp,
+        place: place,
+        updatedAt: updatedAt
       }
     })
 }
 
-// show error message when location isn't found
 function displayLocNotFound() {
-  // clears any previous weather info
   weatherContainer.innerHTML = "";
-  // create h2, add error msg, and add to page
-  const errMsg = document.createElement('h2')
+  var errMsg = document.createElement('h2')
   errMsg.textContent = "Location not found"
   weatherContainer.appendChild(errMsg)
 }
 
-// updates HTML to display weather info
 function displayWeatherInfo(weatherObj) {
-  // clears any previous weather info
+ 
   weatherContainer.innerHTML = "";
 
-  // inserts a linebreak <br> to weather section tag
   function addBreak() {
     weatherContainer.appendChild(
       document.createElement('br')
     )
   }
 
-  // weather location element
-  const placeName = document.createElement('h2')
+ 
+  var placeName = document.createElement('h2')
   placeName.textContent = weatherObj.place
   weatherContainer.appendChild(placeName)
 
-  // map link element based on lat/long
-  const whereLink = document.createElement('a')
+
+  var whereLink = document.createElement('a')
   whereLink.textContent = "Click to view map"
   whereLink.href = "https://www.google.com/maps/search/?api=1&query=" + weatherObj.coords
   whereLink.target = "__BLANK"
   weatherContainer.appendChild(whereLink)
 
-  // weather icon img
-  const icon = document.createElement('img')
+
+  var icon = document.createElement('img')
   icon.src = weatherObj.iconUrl
   weatherContainer.appendChild(icon)
 
-  // weather description
-  const description = document.createElement('p')
+  var description = document.createElement('p')
   description.textContent = weatherObj.description
   description.style.textTransform = 'capitalize'
   weatherContainer.appendChild(description)
 
   addBreak()
 
-  // current temperature
-  const temp = document.createElement('p')
+
+  var temp = document.createElement('p')
   temp.textContent = "Current: " +
     weatherObj.actualTemp +
     "° F"
   weatherContainer.appendChild(temp)
 
 
-  // "feels like" temperature
-  const feelsLikeTemp = document.createElement('p')
+  var feelsLikeTemp = document.createElement('p')
   feelsLikeTemp.textContent = "Feels like: " +
     weatherObj.feelsLikeTemp +
     "° F"
@@ -137,8 +119,8 @@ function displayWeatherInfo(weatherObj) {
 
   addBreak()
 
-  // time weather was last updated
-  const updatedAt = document.createElement('p')
+
+  var updatedAt = document.createElement('p')
   updatedAt.textContent = "Last updated: " +
     weatherObj.updatedAt.toLocaleTimeString(
       'en-US',
