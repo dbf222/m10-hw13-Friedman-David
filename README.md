5208683759c1cdd1f6d975667085a1f20a531e71
David Friedman <dbf22@tampabay.rr.com>
2021-08-06 11:32:49 -0400

    Tried my best

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # m10-hw13-lastname-firstname
+
+
+#ES6 RefactorBackground
+
+For this assignment you are being tasked with converting the solution to the Weather Widget assignment from ES5 JavaScript to ES6+.
+
+##Setup
+Please create a git repository titled m10-hw13-lastname-firstname and clone the repo to your computer. Copy the files from the unsolved folder into the root directory of your cloned repository. Write your code in the index.js file that has been provided.
+
+There are tests included in this assignment. You can run the tests using the index.html file with the live server extension.
+
+##Instructions
+Your assignment is to translate the ES5 JavaScript code in index.js to use ES6+ features. The application is functioning as is, and should function exactly the same when the assignment is completed. To aid your efforts, tests have been added to the application that cover all the critical features of the app.
+
+When converting from ES5 to ES6+, you will need to do each of the following at least once:
+1.  Convert a var declaration to const or let where appropriate.
+2.  Convert a promise-based function (a function call with .then) to instead use async/await.
+3.  Convert a function declaration into an arrow function.
+4.  Convert a string concatenation to instead use template literals and string interpolation.
+5.  Convert some object-related code to use ES6 destructuring.
+
+You are NOT required to convert the entire index.js file to use ES6, although you may if you wish. To receive full credit, you only have to use each of the above features once in your submission. All tests should pass to indicate no changes to functionality of the assignment.
+
+To run the tests:Click the "Run Tests" button in the bottom right corner of the web page. If you would like to run the tests again, reload the page and click the button again.
+
+##Deployment
+Your code must be deployed to GitHub Pages. To deploy a repository to GitHub pages you must1.  Ensure your repository has an index.html file in the root directory.2.  Navigate to the settings section of the repository.3.  Click on pages in the left navigation menu.4.  Under source click the dropdown and select your master or main branch.5.  Click save.Your site should be deployed to <your github username>.github.io/<your repository name> in 5-10 minutes.SubmissionPlease submit both a link to your repository and a link to the live site. Also please include any comments on stumbling blocks or difficulties encountered while completing the assignment.
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+  // // Convert a var declaration to const or let where appropriate.
+  // 2.  Convert a promise-based function (a function call with .then) to instead use async/await.
+  // 3.  Convert a function declaration into an arrow function.
+  // 4.  Convert a string concatenation to instead use template literals and string interpolation.
+  // 5.  Convert some object-related code to use ES6 destructuring
+  // //
+
+
+
 // capture references to important DOM elements
-var weatherContainer = document.getElementById('weather');
-var formEl = document.querySelector('form');
-var inputEl = document.querySelector('input');
+const weatherContainer = document.getElementById('weather');
+const formEl = document.querySelector('form');
+const inputEl = document.querySelector('input');
 
 
-formEl.onsubmit = function(e) {
+formEl.onsubmit = function(e) => {
   // prevent the page from refreshing
   e.preventDefault();
 
   // capture user's input from form field
-  var userInput = inputEl.value.trim()
+  const userInput = inputEl.value.trim()
   // abort API call if user entered no value
   if (!userInput) return
   // call the API and then update the page
@@ -22,12 +31,13 @@ formEl.onsubmit = function(e) {
 }
 
 // calls the OpenWeather API and returns an object of weather info
-function getWeather(query) {
+async function getWeather(query) {
   // default search to USA
   if (!query.includes(",")) query += ',us'
   // return the fetch call which returns a promise
   // allows us to call .then on this function
-  return fetch(
+  var res = await
+   fetch(
     'https://api.openweathermap.org/data/2.5/weather?q=' +
     query +
     '&units=imperial&appid=6efff70fe1477748e31c17d1c504635f'
@@ -39,24 +49,26 @@ function getWeather(query) {
       // location not found, throw error/reject promise
       if (data.cod === "404") throw new Error('location not found')
       // create weather icon URL
-      var iconUrl = 'https://openweathermap.org/img/wn/' +
+      const iconUrl = 'https://openweathermap.org/img/wn/' +
         data.weather[0].icon +
         '@2x.png'
-      var description = data.weather[0].description
-      var actualTemp = data.main.temp
-      var feelsLikeTemp = data.main.feels_like
-      var place = data.name + ", " + data.sys.country
+      const description = data.weather[0].description
+      const actualTemp = data.main.temp
+      const feelsLikeTemp = data.main.feels_like
+      const place = data.name + ", " + data.sys.country
       // create JS date object from Unix timestamp
-      var updatedAt = new Date(data.dt * 1000)
+      const updatedAt = new Date(data.dt * 1000)
+
+
       // this object is used by displayWeatherInfo to update the HTML
       return {
         coords: data.coord.lat + ',' + data.coord.lon,
-        description: description,
-        iconUrl: iconUrl,
-        actualTemp: actualTemp,
-        feelsLikeTemp: feelsLikeTemp,
-        place: place,
-        updatedAt: updatedAt
+        description,
+     iconUrl,
+         actualTemp,
+      feelsLikeTemp,
+      place,
+       updatedAt
       }
     })
 }
@@ -66,7 +78,7 @@ function displayLocNotFound() {
   // clears any previous weather info
   weatherContainer.innerHTML = "";
   // create h2, add error msg, and add to page
-  var errMsg = document.createElement('h2')
+  const errMsg = document.createElement('h2')
   errMsg.textContent = "Location not found"
   weatherContainer.appendChild(errMsg)
 }
@@ -84,24 +96,24 @@ function displayWeatherInfo(weatherObj) {
   }
 
   // weather location element
-  var placeName = document.createElement('h2')
+  const placeName = document.createElement('h2')
   placeName.textContent = weatherObj.place
   weatherContainer.appendChild(placeName)
 
   // map link element based on lat/long
-  var whereLink = document.createElement('a')
+  const whereLink = document.createElement('a')
   whereLink.textContent = "Click to view map"
   whereLink.href = "https://www.google.com/maps/search/?api=1&query=" + weatherObj.coords
   whereLink.target = "__BLANK"
   weatherContainer.appendChild(whereLink)
 
   // weather icon img
-  var icon = document.createElement('img')
+  const icon = document.createElement('img')
   icon.src = weatherObj.iconUrl
   weatherContainer.appendChild(icon)
 
   // weather description
-  var description = document.createElement('p')
+  const description = document.createElement('p')
   description.textContent = weatherObj.description
   description.style.textTransform = 'capitalize'
   weatherContainer.appendChild(description)
@@ -109,14 +121,15 @@ function displayWeatherInfo(weatherObj) {
   addBreak()
 
   // current temperature
-  var temp = document.createElement('p')
+  const temp = document.createElement('p')
   temp.textContent = "Current: " +
     weatherObj.actualTemp +
     "° F"
   weatherContainer.appendChild(temp)
 
+
   // "feels like" temperature
-  var feelsLikeTemp = document.createElement('p')
+  const feelsLikeTemp = document.createElement('p')
   feelsLikeTemp.textContent = "Feels like: " +
     weatherObj.feelsLikeTemp +
     "° F"
@@ -125,7 +138,7 @@ function displayWeatherInfo(weatherObj) {
   addBreak()
 
   // time weather was last updated
-  var updatedAt = document.createElement('p')
+  const updatedAt = document.createElement('p')
   updatedAt.textContent = "Last updated: " +
     weatherObj.updatedAt.toLocaleTimeString(
       'en-US',
